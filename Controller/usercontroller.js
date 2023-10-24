325dac45ca08f525f40e97b52d35fd6df77f5f91
Aparna <manikandanaparna18@gmail.com>
2023-10-24 09:16:49 +0530

    pagination done

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -1,14 +1,14 @@
 const userRoute = require("../Router/userRouter");
 const userController = require("../Controller/usercontroller");
 const User = require("../model/userModel");
-const Cart = require('../model/cartModel')
-const Address = require('../model/addressModel')
-const Category = require('../model/categoryModel')
-const Product = require('../model/productModel')
-const Wishlist = require('../model/wishlistModel')
-const Order = require('../model/orderModel')
-const mongoose = require('mongoose')
- const session = require("express-session");
+const Cart = require("../model/cartModel");
+const Address = require("../model/addressModel");
+const Category = require("../model/categoryModel");
+const Product = require("../model/productModel");
+const Wishlist = require("../model/wishlistModel");
+const Order = require("../model/orderModel");
+const mongoose = require("mongoose");
+const session = require("express-session");
 const bcrypt = require("bcrypt");
 
 const authToken = process.env.AUTH_TOKEN;
@@ -29,21 +29,19 @@ exports.loadLogin = async (req, res) => {
   }
 };
 
-
-
-let d ;
+let d;
 //login page
 exports.Login = async (req, res) => {
   try {
     const { mobile, password } = req.body;
     console.log(mobile, password);
     const userFound = await User.findOne({ mobile });
-    d=mobile;
+    d = mobile;
     if (userFound) {
       // console.log("user found,,,,");
-      if(userFound.isBlocked==true){
-        req.app.locals.specialContext = "You have been blocked."
-        return res.redirect('/login')
+      if (userFound.isBlocked == true) {
+        req.app.locals.specialContext = "You have been blocked.";
+        return res.redirect("/login");
       }
       const hashedpassword = userFound.password;
       const pswdmatch = await bcrypt.compare(password, hashedpassword);
@@ -52,9 +50,9 @@ exports.Login = async (req, res) => {
         req.session.userId = userFound._id;
         // console.log("jjj"+req.session.userId);
         return res.redirect("/");
-      } 
-      else{
-        req.app.locals.specialContext = "mobile number and password are invalid"
+      } else {
+        req.app.locals.specialContext =
+          "mobile number and password are invalid";
         res.redirect("/login");
       }
     } else {
@@ -80,25 +78,24 @@ exports.LoadRegister = async (req, res) => {
 let details;
 exports.signUp = async (req, res) => {
   try {
-    const { username, mobile,email, password } = req.body;
-    details = { username, mobile,email, password };
+    const { username, mobile, email, password } = req.body;
+    details = { username, mobile, email, password };
     console.log(username, mobile, password);
     const findUser = await User.findOne({ mobile });
     if (findUser) {
       req.app.locals.specialContext = "Mobile number already exist";
-      res.redirect("/login")
-       console.log("user already exists");
+      res.redirect("/login");
+      console.log("user already exists");
     } else {
       client.verify.v2.services(serviceId).verifications.create({
         to: `+91${mobile}`,
         channel: "sms",
       });
-      req.session.otpPageForSignUp = true
+      req.session.otpPageForSignUp = true;
       //res.redirect("/otp");
       console.log(req.session.otpPageForSignUp);
-      res.redirect(`/verifyOtp?mobile=${mobile}`)
+      res.redirect(`/verifyOtp?mobile=${mobile}`);
     }
-
   } catch (error) {
     console.log(error);
   }
@@ -109,10 +106,10 @@ exports.loadOtp = async (req, res) => {
   try {
     var context = req.app.locals.specialContext;
     req.app.locals.specialContext = null;
-    if(req.session.otpPageForSignUp == true){
-        res.render("otp",{context});
-    }else{
-        res.redirect('/register')
+    if (req.session.otpPageForSignUp == true) {
+      res.render("otp", { context });
+    } else {
+      res.redirect("/register");
     }
   } catch (error) {
     console.log(error);
@@ -134,20 +131,19 @@ exports.verifyOtp = async (req, res) => {
         username,
         mobile,
         password: hashedPassword,
-        joinedDate : new Date()
+        joinedDate: new Date(),
       });
       console.log("data got it");
       const userData = await newUser.save();
-      if (userData){
+      if (userData) {
         // req.app.locals.specialContext = "Registration successful. Please login";
         res.redirect("/");
         console.log("registartion successful");
-
-      } 
+      }
     } else {
       console.log("otp error");
-       req.app.locals.specialContext="invalid otp"
-    res.redirect('/otp')
+      req.app.locals.specialContext = "invalid otp";
+      res.redirect("/otp");
     }
   } catch (error) {
     console.log(error);
@@ -155,149 +151,219 @@ exports.verifyOtp = async (req, res) => {
 };
 
 //rendering home page
-exports.LoadHome = async (req, res,next) => {
+exports.LoadHome = async (req, res, next) => {
   try {
-    const category = await Category.find({isListed:true})
-    res.render("index",{category});
+    const category = await Category.find({ isListed: true });
+    res.render("index", { category });
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
 };
 
 //shop page rendering
-exports.LoadShopPage= async (req,res,next)=>{
-try {
-  const user = req.session.userId;
-  // const productId =req.query.productId;
-  // console.log("productId",productId);
-  const wishlist = await Wishlist.findOne({user})
-  // console.log(wishlist);
-  const category =await Category.find({isListed:true})
-  console.log(category);
-  
-  // if (category.length > 0) {
-  //   var brand = category[0].brands;
-  //   console.log(brand);
-  // }
-  const products = await Product.find({isListed:true})
-  
-  res.render('shop',{products,category,wishlist,brands: category[0].brands})
-} catch (error) {
-  console.log(error);
-  next(error)
-}
-}
+exports.LoadShopPage = async (req, res, next) => {
+  try {
+    const user = req.session.userId;
 
-//single page 
-exports.singleProduct = async (req,res,next)=>{
+    //get users wishlist
+    const wishlist = await Wishlist.findOne({ user });
+
+    //category for listing
+    const category = await Category.find({ isListed: true });
+
+    //filter
+    //category filter
+    let categorySelected = req.query.category || "";
+    let categoryFind = await Category.find(
+      { category: { $in: categorySelected } },
+      { _id: 1 }
+    );
+    if (!categoryFind.length) {
+      categoryFind = await Category.find({}, { _id: 1 });
+    }
+
+    let categoryStrings = categoryFind.map((val) => val._id.toString());
+    let categoryToFront = req.query.category || "";
+
+    //brands filter
+    //for db search
+    let brandSelected = req.query.brand || category[0].brands;
+    //for front end check box selection
+    let brandsToFront = req.query.brand || "";
+
+      //sort by price setting
+      let sort = req.query.sort || {addedDate : 1}
+    //   console.log(sort);
+      let sortToFront = sort
+      if(sort == 1){
+          sort = {sellingPrice:1} 
+      }else if(sort == -1){
+          sort = {sellingPrice:-1}
+      }
+
+
+    //search filter
+    let search = req.query.search || ''
+
+
+    //pagination
+    let limit = 6;
+    const page = req.query.page || 1;
+
+
+
+  const products = await Product.find({
+      isListed: true,
+      brand: brandSelected,
+      category: categoryStrings,
+      $or:[
+        {productname:{$regex:search,$options:'i'}},
+        {brands :{$regex:search,$options:'i'}}
+      ]
+    }).limit(limit*1).skip((page-1)*limit).sort(sort);
+
+    
+  const productsCount = await Product.find({
+    isListed: true,
+    brand: brandSelected,
+    category: categoryStrings,
+    $or:[
+      {productname:{$regex:search,$options:'i'}},
+      {brands :{$regex:search,$options:'i'}}
+    ]
+  }).countDocuments()
+
+
+    res.render("shop", {
+      products,
+      category,
+      wishlist,
+      brands: category[0].brands,
+      brandsToFront,
+      categoryToFront,
+      search,
+      sortToFront,
+      page,
+      limit,
+      totalPages: Math.ceil(productsCount/limit)
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+//single page
+exports.singleProduct = async (req, res, next) => {
   try {
     const user = req.session.userId;
     const id = req.query.id;
-    let productFound,wishlistFound
-    if(user){
-       productFound =await Cart.findOne({user},{product:{$elemMatch:{product_id:id}}})
-       wishlistFound = await Wishlist.findOne({user},{product:{$elemMatch:{product_id:id}}})
+    let productFound, wishlistFound;
+    if (user) {
+      productFound = await Cart.findOne(
+        { user },
+        { product: { $elemMatch: { product_id: id } } }
+      );
+      wishlistFound = await Wishlist.findOne(
+        { user },
+        { product: { $elemMatch: { product_id: id } } }
+      );
     }
     console.log(productFound);
 
-      console.log("id of single page"+id)
-    const singleProduct = await Product.findById({_id:id}).populate('category');
+    console.log("id of single page" + id);
+    const singleProduct = await Product.findById({ _id: id }).populate(
+      "category"
+    );
     //  console.log("singleproduct data"+singleProduct);
-    if(singleProduct){
-      res.render('singleproduct',{singleProduct,productFound,wishlistFound})
+    if (singleProduct) {
+      res.render("singleproduct", {
+        singleProduct,
+        productFound,
+        wishlistFound,
+      });
     }
-    
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
- 
-}
-
+};
 
 // user profile page -GET
-exports.loadUserProfile= async (req ,res,next)=> {
+exports.loadUserProfile = async (req, res, next) => {
   try {
-     
     const mobile = d;
     console.log(mobile);
-    const user = await User.findOne({mobile})
+    const user = await User.findOne({ mobile });
     // req.session.userId = user._id
     console.log(user);
-    res.render('userProfile',{user})
+    res.render("userProfile", { user });
     console.log("rendering userprofile.....");
- }  
-   catch (error) {
-    console.log(error)
-    next(error)
-  }
-}
-
-
-// user profile page - POST
-exports.profile = async (req,res)=>{
-  try {
-    
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-//load edit profile - GET
-exports.LoadUserEditProfile  = async (req,res,next)=>{
-  try {
-    const id = req.query._id
-    console.log(id);
-    const editData = await User.findOne({_id:id})
-    console.log(editData);
-    res.render('editProfile',{data:editData})
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
-}
+};
+
+// user profile page - POST
+exports.profile = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//load edit profile - GET
+exports.LoadUserEditProfile = async (req, res, next) => {
+  try {
+    const id = req.query._id;
+    console.log(id);
+    const editData = await User.findOne({ _id: id });
+    console.log(editData);
+    res.render("editProfile", { data: editData });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 //edit profile -POST
-exports.editProfile = async (req,res)=>{
+exports.editProfile = async (req, res) => {
   try {
     const id = req.body.id;
     // console.log("id of the id"+id)
-    const {name,mobile,email} = req.body;
+    const { name, mobile, email } = req.body;
     // console.log(name,mobile,email);
-    const data = await User.findOne({_id:id})
-    if(data){
-      await User.findByIdAndUpdate({_id:id},{$set:{name,mobile,email}})
-      console.log("User profile edited..."+data);
-      res.redirect('/Profile')
-    }
-    else{
+    const data = await User.findOne({ _id: id });
+    if (data) {
+      await User.findByIdAndUpdate(
+        { _id: id },
+        { $set: { name, mobile, email } }
+      );
+      console.log("User profile edited..." + data);
+      res.redirect("/Profile");
+    } else {
       console.log("not edited data");
     }
   } catch (error) {
     console.log(error);
   }
-}
-
-
-
-
-
+};
 
 //user logout
-exports.userLogOut = async(req,res,next)=>{
+exports.userLogOut = async (req, res, next) => {
   try {
     req.session.destroy((err) => {
       if (err) {
         console.error(err);
       } else {
-        res.redirect('/login');
-        console.log('User logged out');
+        res.redirect("/login");
+        console.log("User logged out");
       }
     });
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
-}
+};
